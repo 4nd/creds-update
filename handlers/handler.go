f97ddc9d425458794a3afc4fea5c01963f028e57
Andy Graham <andy@4nd.dev>
2024-09-11 21:14:13 +0100

    move template load to render function

--- handlers/handler.go
+++ handlers/handler.go
@@ -3,7 +3,7 @@ package handlers
 import (
 	"creds-update/resources"
 	"database/sql"
-	"log"
+	"fmt"
 	"net/http"
 	"os"
 )
@@ -32,12 +32,12 @@ func (h *RouteHandler) render(
 
 	view, ok := resources.Views[viewFile]
 	if !ok {
-		log.Printf("view %s not found", viewFile)
+		fmt.Printf("view %s not found", viewFile)
 		return
 	}
 
 	if err := view.Execute(resp, data); err != nil {
-		log.Println(err)
+		fmt.Println(err)
 	}
 }
 
