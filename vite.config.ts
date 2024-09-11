8a15da122f35538727f4bbdea948621988a8f536
Andy Graham <andy@4nd.dev>
2024-09-11 21:05:49 +0100

    remove attempt to get vite url

--- vite.config.ts
+++ vite.config.ts
@@ -15,21 +15,8 @@ function hotFilePlugin(): Plugin[] {
             server.httpServer?.once('listening', () => {
                 const address = server.httpServer?.address()
                 if (typeof address === 'object') {
-                    const config = resolvedConfig
-                    const configHmrProtocol = typeof config.server.hmr === 'object' ? config.server.hmr.protocol : null
-                    const clientProtocol = configHmrProtocol ? (configHmrProtocol === 'wss' ? 'https' : 'http') : null
-                    const serverProtocol = config.server.https ? 'https' : 'http'
-                    const protocol = clientProtocol ?? serverProtocol
 
-                    const configHmrHost = typeof config.server.hmr === 'object' ? config.server.hmr.host : null
-                    const configHost = typeof config.server.host === 'string' ? config.server.host : null
-                    const serverAddress = address.family === '6' || address.family === 'IPv6' ? `[${address.address}]` : address.address
-                    const host = configHmrHost ?? configHost ?? serverAddress
-
-                    const configHmrClientPort = typeof config.server.hmr === 'object' ? config.server.hmr.clientPort : null
-                    const port = configHmrClientPort ?? address.port
-
-                    fs.writeFileSync(hotFile, `${protocol}://${host}:${port}`)
+                    fs.writeFileSync(hotFile, "vite is hot")
                     if (!exitHandlersBound) {
                         const clean = () => {
                             if (fs.existsSync(hotFile)) {
